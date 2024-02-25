517d6b122db6928de6135a5b65ed8cd89646b2bf
Mohamed abdraboh <mohamedabdraboh015@gmail.com>
2024-02-25 07:51:09 +0200

    lab7 completed

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,97 +1,136 @@
 package bstmap;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
-    private static class BSTNode<K extends Comparable<K>> {
-        K data;
+    private class BSTNode {
+        K key;
+        V value;
         BSTNode left;
         BSTNode right;
 
-        BSTNode(K data, BSTNode left, BSTNode right) {
-            this.data = data;
+        public BSTNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        public BSTNode(K key, V value, BSTNode left, BSTNode right) {
+            this.key = key;
+            this.value = value;
             this.left = left;
             this.right = right;
         }
 
-        boolean containsData(K key) {
-            if (this.data == key)
-                return true;
 
-            else if (this.data.compareTo(key) < 0)
-                right.containsData(key);
-            else
-                left.containsData(key);
-
-            return false;
-        }
     }
 
-    private BSTNode<K> head;
+    private BSTNode root;
     private int size;
 
     public BSTMap() {
+        root = null;
         size = 0;
-    }
-
-    public BSTMap(BSTNode head) {
-        this.head = head;
-        this.size = 1;
-    }
-
-    public void printInOrder() {
-
     }
 
     @Override
     public void clear() {
-        head = null;
+        root = null;
         size = 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return head.containsData(key);
-
+        return getKey(root, key) != null;
     }
+
 
     @Override
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(BSTNode node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) < 0)
+            return get(node.left, key);
+        else if (key.compareTo(node.key) > 0)
+            return get(node.right, key);
+        else return node.value;
+    }
+
+    private K getKey(BSTNode node, K key) {
+        if (node == null)
+            return null;
+        if (key.compareTo(node.key) < 0)
+            return getKey(node.left, key);
+        else if (key.compareTo(node.key) > 0)
+            return getKey(node.right, key);
+        else return node.key;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+        BSTNode node = put(root, key, value);
+        if (root == null)
+            root = node;
+        size++;
+        System.out.println();
+    }
 
+    private BSTNode put(BSTNode node, K key, V value) {
+        if (node == null)
+            return new BSTNode(key, value);
+
+        if (key.compareTo(node.key) < 0)
+            node.left = put(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = put(node.right, key, value);
+
+        return node;
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(BSTNode node) {
+        if (node == null)
+            return;
+        System.out.print(node.value);
+        printInOrder(node.left);
+        printInOrder(node);
+        printInOrder(node.right);
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        throw new UnsupportedOperationException();
+
     }
 
     @Override
     public V remove(K key) {
-        return null;
+        throw new UnsupportedOperationException();
+
     }
 
     @Override
     public V remove(K key, V value) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
-
-
 }
